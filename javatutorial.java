3b5e0ff116777fbb11804626ec21c21f896feac7
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-09 20:20:45 +0530

    first program in java

---
+++ javatutorial.java
@@ -0,0 +1,18 @@
+package tutorial;
+class Aman
+{
+	Aman()
+	{
+		System.out.println("in A");
+	}
+}
+
+public class javatutorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi I am first one in java program");
+Aman a=new Aman();
+	}
+
+}
