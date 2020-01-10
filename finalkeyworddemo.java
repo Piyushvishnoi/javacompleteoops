2a2d4ac9a66a724d24afe1bd2668a928e98abbd4
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-10 16:52:58 +0530

    final keyword use in java

---
+++ finalkeyworddemo.java
@@ -0,0 +1,21 @@
+package tutorial;
+class f
+{
+	final Integer i; //i will become constant when assigned once
+	f()
+	{
+		i=10;
+		System.out.println(i);
+		
+	}
+	
+}
+
+public class finalkeyworddemo {
+
+	public static void main(String[] args) {
+		
+f a=new f();
+	}
+
+}
