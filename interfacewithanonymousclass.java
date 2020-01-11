355b389aa8f3a2d3d5a673d2a5f33319183051c5
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-11 19:10:15 +0530

    implementing interface using anonymous class

---
+++ interfacewithanonymousclass.java
@@ -0,0 +1,20 @@
+package tutorial;
+interface demo
+{
+	void show();
+}
+public class interfacewithanonymousclass {
+
+	public static void main(String[] args) {
+		demo d=new demo()     // we cannot make object of interface but using anonymous class we can make its object
+				{             //hence we don't have to define an implementor class especialy for implementing interface
+public void show()
+{
+	System.out.print("implementing interface using anonymous class");
+}
+				};
+				d.show();
+
+	}
+
+}
