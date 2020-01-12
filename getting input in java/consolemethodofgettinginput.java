346c21a9c9260cd60a79397fd2696c70f7dc070b
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-13 01:05:29 +0530

    console to get input

---
+++ getting input in java/consolemethodofgettinginput.java
@@ -0,0 +1,12 @@
+package waysofinputinjava;
+import java.io.Console;
+public class consolemethodofgettinginput {
+
+	public static void main(String args[]) {
+		//Console obj=System.console();
+		String nam= System.console().readline("enter name");
+		System.out.println(nam);
+
+	}
+
+}
