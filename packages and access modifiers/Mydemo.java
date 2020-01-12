8376c881786bedbc65931333fd4e664a09ced05e
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-12 12:15:44 +0530

    just a class with variables

---
+++ packages and access modifiers/Mydemo.java
@@ -0,0 +1,7 @@
+package p2;
+public class Mydemo {
+int i=0; // default can be accessed only in same package
+public int a=1; //can be accessed anywhere
+protected int b=2; //can be accessed in same package and also in different package provided there must be a subclass
+private int c=3;
+}
