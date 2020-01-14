7754e4f22cfae232fa98b042c0d47d0e526c0ec1
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-14 15:44:10 +0530

    threading demo

---
+++ threads in java/threadingdemo.java
@@ -0,0 +1,32 @@
+package thread;
+class Hi extends Thread
+{
+	public void run()
+	{
+	for(Integer i=0;i<3;i++)
+	{
+	System.out.println("Hi");	
+	}
+	}
+}
+class hello extends Thread
+{
+	public void run()
+	{
+	for(Integer i=0;i<3;i++)
+	{
+	System.out.println("Hello");	
+	}
+	}
+}
+public class threadingdemo {
+	public static void main(String[] args)
+	{
+		
+Hi obj1=new Hi();
+hello obj2=new hello();
+obj1.start();
+
+obj2.start();
+	}
+}
