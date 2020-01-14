a3556c32e666fc3347a3e49c33f35a776abf350a
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-14 15:40:47 +0530

    command line argument with some experiment

---
+++ command line argument in java/Commandlineargumentdemo.java
@@ -0,0 +1,12 @@
+
+public class Commandlineargumentdemo {
+
+	public static void main(String[] args) {
+		System.out.println("the argumemnts are:");
+			Integer a=Integer.parseInt("43");
+		
+		//else
+			System.out.println(a);
+	}
+
+}
