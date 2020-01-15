0ebcc35106c12e20eb71aff8ca4caaa03463bb02
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-15 16:49:45 +0530

    getting and setting priorities of threads
    using get and set methods

---
+++ threads in java/Prioritiesinthreads.java
@@ -0,0 +1,48 @@
+package thread;
+public class Prioritiesinthreads {
+
+	public static void main(String[] args) {
+		Runnable obj1=() ->
+		{
+			for(Integer i=0;i<3;i++)
+			{
+			System.out.println("Hi");	
+		try { Thread.sleep(1000);} catch(Exception e) {}
+			}
+			}
+	;
+	Runnable obj2=() ->      // since runnable is a functional interface so we can use lambda expression
+		{
+		for(Integer i=0;i<3;i++)
+		{
+		System.out.println("Hello");	
+		try { Thread.sleep(1000);} catch(Exception e) {}
+		}
+		}
+			;
+			Thread t1=new Thread(obj1);
+			Thread t2=new Thread(obj2);
+			t1.setPriority(Thread.MAX_PRIORITY);
+			t2.setPriority(1);
+			System.out.println(t1.getPriority()); // getting priority of threads
+			System.out.println(t2.getPriority());
+			t1.start();
+			try { Thread.sleep(10);} catch(Exception e) {}
+			t2.start();
+			try {
+				t1.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(t2.isAlive());
+			try {
+				t2.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(t1.isAlive());
+			System.out.println("in main thread");
+	}
+}
