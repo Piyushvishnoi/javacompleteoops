29c9a92128064a620bcb4406b5a0ad86e810edcd
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-16 17:25:02 +0530

    collection demo in java with arraylist

---
+++ collections in java/collectiondemo.java
@@ -0,0 +1,17 @@
+//we cannot use index no in collection and arraylist
+package collectionandgenerics;
+import java.util.ArrayList;  //package for ArrayList
+import java.util.Collection; //package for collection
+
+public class collectiondemo {
+
+	public static void main(String[] args) {
+		Collection values=new ArrayList();
+	values.add(4);
+	values.add(2);
+	values.add(9);
+System.out.println(values);  //this will print all the values
+
+	}
+
+}
