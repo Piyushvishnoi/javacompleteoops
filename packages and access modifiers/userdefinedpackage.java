33fceb34fbf23649b07462eeeabaedb913173e95
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-19 20:09:04 +0530

    user defined package

---
+++ packages and access modifiers/userdefinedpackage.java
@@ -0,0 +1,12 @@
+import userdefined.*;
+public class userdefinedpackage {
+
+	public static void main(String[] args) {
+				String strr="Piyush";
+			myclass obj=new myclass();
+			myclass.show(strr);
+
+		}
+
+
+	}
