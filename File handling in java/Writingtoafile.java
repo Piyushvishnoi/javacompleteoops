c06a39899c7f565637c50c2b453d810cc660c35c
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-20 23:06:35 +0530

    writing to a file

---
+++ File handling in java/Writingtoafile.java
@@ -0,0 +1,24 @@
+package filehandling;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Writingtoafile {
+
+	public static void main(String[] args) {
+try
+{
+	FileWriter obj=new FileWriter("File1.txt");
+	obj.write("I am mad");
+	obj.close();
+	System.out.println("Successfully written to a file");
+}
+catch(IOException e)
+{
+System.out.println("An error occured");
+e.printStackTrace();
+}
+
+	}
+
+}
