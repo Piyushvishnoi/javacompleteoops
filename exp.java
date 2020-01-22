16736dbac9b0e5b9a4d499bb5d4d4e841e835c6d
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-22 23:20:44 +0530

    string and string buffer difference

---
+++ exp.java
@@ -0,0 +1,14 @@
+
+public class exp {
+
+	public static void main(String[] args) {
+String s=" man ";
+System.out.println(s);
+String a=s.trim();
+System.out.println(a);
+StringBuffer sb=new StringBuffer(" man ");
+
+
+	}
+
+}
