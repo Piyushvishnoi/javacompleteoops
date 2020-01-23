b5d6199465697ec140742c99678a805ca9fda6d5
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-23 22:32:10 +0530

    Arraylist vs linked list in java

---
+++ linked list/arraylistvslinkedlist.java
@@ -0,0 +1,21 @@
+package linkedlistjava;
+		import java.util.*;    
+		class arraylistvslinkedlist{    
+		 public static void main(String args[]){    
+		     
+		  List<String> al=new ArrayList<String>();//creating arraylist    
+		  al.add("Ravi");//adding object in arraylist    
+		  al.add("Vijay");    
+		  al.add("Ravi");    
+		  al.add("Ajay");    
+		    
+		  List<String> al2=new LinkedList<String>();//creating linkedlist    
+		  al2.add("James");//adding object in linkedlist    
+		  al2.add("Serena");    
+		  al2.add("Swati");    
+		  al2.add("Junaid");    
+		    
+		  System.out.println("arraylist: "+al);  
+		  System.out.println("linkedlist: "+al2);  
+		 }    
+		}    
