3b713a837ca97ead56b2437131749ae3feaf65f6
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-25 00:10:56 +0530

    Update linkedlistdemo.java

--- linked list/linkedlistdemo.java
+++ linked list/linkedlistdemo.java
@@ -1,12 +1,13 @@
 package linkedlistjava;
-
+//in java linked list can have duplicate items
+//there is no synchronization in linked list
 import java.util.Iterator;
 import java.util.LinkedList;
 
 public class linkedlistdemo {
 
 	public static void main(String[] args) {
-	LinkedList <Integer> obj=new LinkedList <Integer>();
+	LinkedList <Integer> obj=new LinkedList <Integer>();  //linked list is connected to generics because we have restreicted the type of data here
 	obj.add(4);
 	obj.add(5);
 	obj.add(1);
