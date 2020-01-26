b5d9ec6fdd1fcd84f9b09451320cc5062544ab54
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-26 16:58:16 +0530

    Update linkedhashmap.java

--- list interface over collection/linkedhashmap.java
+++ list interface over collection/linkedhashmap.java
@@ -1,6 +1,7 @@
 package setinterface;
 //LinkedHashMap is a Hash table and linked list implementation of the Map interface, with predictable iteration order.
 //This implementation differs from HashMap in that it maintains a doubly-linked list running through all of its entries. 
+//This linked list defines the iteration ordering, which is normally the order in which keys were inserted into the map (insertion-order).
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
