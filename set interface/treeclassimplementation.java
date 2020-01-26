dada3727d0a6106d10c1ea07fa86b6d1301e814f
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-26 16:07:22 +0530

    Update treeclassimplementation.java

--- set interface/treeclassimplementation.java
+++ set interface/treeclassimplementation.java
@@ -6,12 +6,17 @@ import java.util.TreeSet;
 public class treeclassimplementation {
 
 	public static void main(String[] args) {
-		Set <Integer> values=new TreeSet<>();  //in tree set we get ascending  order data
+		Set <Integer> values=new HashSet<>();  //in Hash set we do not get  ordered data
 		values.add(2);
 		values.add(4);
 		values.add(1);
 		values.add(2); //here duplicate values can't be entered in set
 		for(Integer i:values)
+		{
+			System.out.println(i);
+		}
+		TreeSet <Integer> obj1=new TreeSet<> (values); //here we get ordered values in ascending order
+		for(Integer i:obj1)
 		{
 			System.out.println(i);
 		}
