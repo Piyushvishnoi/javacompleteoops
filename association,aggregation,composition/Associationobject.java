24220aaac5d2442ff78617e4daada8457b328963
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-27 13:55:03 +0530

    Association of objects

---
+++ association,aggregation,composition/Associationobject.java
@@ -0,0 +1,36 @@
+package associationaggregationcomposition;
+//Association is relation between two separate classes which establishes through their Objects
+class A
+{ private String name;
+	A(String name)
+	{
+		this.name=name;
+	}
+	public String getname()
+	{
+		System.out.println("In class A name is: "+ this.name);
+		return this.name;
+	}
+}
+class B
+{ private String name;
+	B(String name)
+	{
+		this.name=name;
+	}
+	public String getname()
+	{
+		System.out.println("In class B name is: "+ this.name);
+		return this.name;
+	}
+}
+
+public class Associationobject {
+
+	public static void main(String[] args) {
+	A a=new A("Piyush");
+	B b=new B("khushi");
+System.out.println("In class C "+ a.getname() + " And " + b.getname()); //asociation takes place between objects
+	}
+
+}
