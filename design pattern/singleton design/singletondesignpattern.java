52a37cfcf823792ea78612fd8a819607333091cd
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-28 15:31:00 +0530

    eager initialisation in singleton

---
+++ design pattern/singleton design/singletondesignpattern.java
@@ -0,0 +1,29 @@
+package designpatterninjava;
+//eager initialization
+//for singleton class,we have to do three things:
+//1:create static instance of that class
+//2:don't allow user to create instance for a class using constructor,i.e
+//we can make default constructor as private
+//3:make a static method which will return that static object 
+public class singletondesignpattern {
+
+	public static void main(String[] args) {
+  A obj1=A.getinstance();   //here we are using same instance that we have created in same class
+  A obj2=A.getinstance();
+
+	}
+
+}
+class A
+{
+	static A obj=new A();
+	private A()
+	{
+		System.out.println("In class A default private constructor");
+	}
+	public static A getinstance()
+	{
+		System.out.println("In get instance function and returning instance");
+		return obj;
+	}
+}
