cb7d1bf7480150332b6a71bfaa370b5f8bf28eac
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-28 16:33:32 +0530

    android,ios class to implement os interface

---
+++ design pattern/factory design pattern/android.java
@@ -0,0 +1,9 @@
+package com.phone;
+
+public class android implements OS {
+public void specs()
+{
+	System.out.print("most powerful os i.e Android");
+}
+
+}
---
+++ design pattern/factory design pattern/ios.java
@@ -0,0 +1,12 @@
+package com.phone;
+
+public class ios implements OS{
+
+	public void specs()
+	{
+		System.out.print("most secured os i.e IOS");
+	}
+		
+	}
+	
+
