76d3e0dea2a0d27e43ca45555d70c1f701affe5e
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-29 18:44:27 +0530

    Update lambdaexpression.java

--- interface in java/lambdaexpression.java
+++ interface in java/lambdaexpression.java
@@ -2,6 +2,7 @@
 //Enable to treat functionality as a method argument, or code as data.
 //A function that can be created without belonging to any class.
 //A lambda expression can be passed around as if it was an object and executed on demand.
+//lambda expressions implement the only abstract function and therefore implement functional interfaces
 interface FuncInterface 
 { 
     // An abstract function 
