f433bb9a38d655722756d8ff74cf39c071cc6c14
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-30 23:34:57 +0530

    Update hashtabledemo.java

--- hash table/hashtabledemo.java
+++ hash table/hashtabledemo.java
@@ -1,5 +1,7 @@
 import java.util.Hashtable;
-
+//Hashtable stores key/value pair in hash table.
+//In Hashtable we specify an object that is used as a key, and the value we want to associate to that key. 
+//The key is then hashed, and the resulting hash code is used as the index at which the value is stored within the table
 public class hashtabledemo {
 
 	public static void main(String[] args) {
