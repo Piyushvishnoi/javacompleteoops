0b0c6a56b49040f66d20abe605aae222db990fe3
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-01-31 21:42:11 +0530

    Bitset in java

---
+++ bitsetdemo.java
@@ -0,0 +1,20 @@
+import java.util.BitSet;
+//
+public class bitsetdemo {
+
+	public static void main(String[] args) {
+	BitSet b=new BitSet();
+	BitSet b1=new BitSet(4);
+	b.set(1);
+	b.set(2);
+b1.set(5);
+b1.set(6);
+b1.set(7);
+b1.set(8);
+b1.set(9);
+System.out.println(b);
+System.out.println(b1);
+
+	}
+
+}
