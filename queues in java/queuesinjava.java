5744eb41bf92a983f2e97ad06de7c20ac974a286
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-01 10:41:30 +0530

    queues in java with basic methods
    add, peek,remove,to find size(size method)

---
+++ queues in java/queuesinjava.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+//queue is an interface which even can be implemented by LINKEDLIST
+public class queuesinjava {
+
+	public static void main(String[] args) {
+	Queue <Integer> obj=new LinkedList<>();
+	for(Integer i=0;i<5;i++)
+	{
+		obj.add(i);
+		
+	}
+	//to show elements of queue
+	System.out.println(obj);
+	//toremove head of the queue
+	int objrem=obj.remove();
+	System.out.println("removed head element is: "+objrem);
+	//after removing queue is
+	System.out.println("after removing an element queue is: "+obj);
+	//to view what is on the head of queue
+	int objhead=obj.peek();
+	System.out.println("what is on the head of queue: "+ objhead);
+	//to find size of the queue
+	int objsize=obj.size();
+	System.out.println("Size of queue is: "+objsize);
+	}
+
+}
