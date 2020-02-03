1928e34547df814ad4d7d3bb5ceaa63d44981c31
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-03 23:46:58 +0530

    Constructor chaining in java using this keyword

---
+++ Constructor chaining/constructorchaininginjava.java
@@ -0,0 +1,33 @@
+
+public class constructorchaininginjava {
+ 
+    // default constructor 1 
+    // default constructor will call another constructor 
+    // using this keyword from same class 
+	constructorchaininginjava() 
+    { 
+        // calls constructor 2 
+        this(5); 
+        System.out.println("The Default constructor"); 
+    } 
+  
+    // parameterized constructor 2 
+	constructorchaininginjava(int x) 
+    { 
+        // calls constructor 3 
+        this(5, 15); 
+        System.out.println(x); 
+    } 
+  
+    // parameterized constructor 3 
+	constructorchaininginjava(int x, int y) 
+    { 
+        System.out.println(x * y); 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        // invokes default constructor first 
+        new constructorchaininginjava(); 
+    } 
+} 
