3dd1e4fb3c248b2f68b96d9883ff5fe0a768d152
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-03 23:48:49 +0530

    Constructor chaining using super keyword

---
+++ Constructor chaining/constructorchianingusingsuperkeyword.java
@@ -0,0 +1,30 @@
+
+public class constructorchianingusingsuperkeyword {
+class A
+{
+	A()
+	{
+		System.out.println("In base class default constructor");
+	}
+	A(String name)
+	{
+		System.out.println("In base class parametrized constructor");
+	}
+	}
+class B extends A
+{
+B()
+{
+	System.out.println("In derived class default constructor");
+}
+B(String name)
+{ super(name);
+	System.out.println("In derived class parametrized constructor");
+}
+}
+	public static void main(String[] args) {
+		B b=new B("Piyush");
+
+	}
+
+}
