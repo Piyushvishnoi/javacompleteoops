2c57b2558a77d296bda95b3ccfdf55bb8ba31f49
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-04 18:36:34 +0530

    Dashcode method in object class

---
+++ object class/hashcodemethod.java
@@ -0,0 +1,10 @@
+package objectclass;
+
+public class hashcodemethod {
+
+	public static void main(String[] args) {
+hashcodemethod obj=new hashcodemethod();
+System.out.println("every object has its own unique code: "+obj.hashCode());
+}
+
+}
