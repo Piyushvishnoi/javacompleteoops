a7d82bd8b8972cd750851f6247ecb23d1866f9c3
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-04 17:44:07 +0530

    testing() in object class

---
+++ object class/tostringmethod.java
@@ -0,0 +1,12 @@
+package objectclass;
+
+public class tostringmethod {
+
+	public static void main(String[] args) {
+		tostringmethod obj=new tostringmethod();
+		System.out.println(obj);
+		
+		System.out.println(obj.toString());
+	}
+
+}
