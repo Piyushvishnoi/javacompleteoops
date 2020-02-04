4bcc9f0182573d282aee8ee53ec81781b34ae0d3
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-04 17:45:58 +0530

    Update tostringmethod.java

--- object class/tostringmethod.java
+++ object class/tostringmethod.java
@@ -6,7 +6,8 @@ public class tostringmethod {
 	public static void main(String[] args) {
 		tostringmethod obj=new tostringmethod();
 		System.out.println(obj);
-		
+//The default toString() method for class Object returns a string consisting of the name of the class of which the object is an instance, 
+//the at-sign character `@’, and the unsigned hexadecimal representation of the hash code of the object.	
 		System.out.println(obj.toString());
 	}
 
