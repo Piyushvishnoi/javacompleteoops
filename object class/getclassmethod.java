198406fd05e9ae5eff6f8b98293f518bfc82e60e
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-04 19:44:37 +0530

    getclass  and finalize method in object class
    garbage collector

--- object class/getclassmethod.java
+++ object class/getclassmethod.java
@@ -1,5 +1,3 @@
-//Returns the class object of “this” object and used to get actual runtime class of the object.
-//It can also be used to get metadata of this class. 
 package objectclass;
 
 public class getclassmethod 
