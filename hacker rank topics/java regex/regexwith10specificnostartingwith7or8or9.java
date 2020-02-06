d2533962c901e8a6ffa37a486906f5bdd8145507
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-06 23:38:01 +0530

    Update regexwith10specificnostartingwith7or8or9.java

--- hacker rank topics/java regex/regexwith10specificnostartingwith7or8or9.java
+++ hacker rank topics/java regex/regexwith10specificnostartingwith7or8or9.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class regexwith10specificnostartingwith7or8or9 {
 
 	public static void main(String[] args) {
+		//start only with 7or8or9
 		System.out.println(Pattern.matches("[789]{1}[0-9]{9}","7654321908"));
 		System.out.println(Pattern.matches("[789]{1}[0-9]{9}","3654321908"));
 
