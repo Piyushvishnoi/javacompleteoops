62a8077cb54e2738dc49a2e6781459976d32ae3b
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-06 23:29:59 +0530

    Update regexwithalphanumericonly.java

--- hacker rank topics/java regex/regexwithalphanumericonly.java
+++ hacker rank topics/java regex/regexwithalphanumericonly.java
@@ -1,11 +1,11 @@
 package hackerranktopics;
-
+//a pattern have to be followed
 import java.util.regex.Pattern;
 
 public class regexwithalphanumericonly {
 
 	public static void main(String[] args) {
-	System.out.println(Pattern.matches("[a-zA-Z0-9]{6}","abcA56"));
+	System.out.println(Pattern.matches("[a-zA-Z0-9]{6}","abcA56")); //a pattern is followed
 	System.out.println(Pattern.matches("[a-zA-Z0-9]{6}","abcA5$"));
 
 	}
