41154563800adbefca50a1672bbf5627088e74fd
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-07 01:35:58 +0530

    use of ?,+and* in regex

--- hacker rank topics/java regex/regexusageofsomesymbols.java
+++ hacker rank topics/java regex/regexusageofsomesymbols.java
@@ -5,13 +5,13 @@ import java.util.regex.Pattern;
 public class regexusageofsomesymbols {
 
 	public static void main(String[] args) {
-	System.out.println(Pattern.matches("[amn]?","a"));
+	System.out.println(Pattern.matches("[amn]?","a")); //true (a or m or n comes one time) 
 	System.out.println(Pattern.matches("[amn]?","am"));
 	System.out.println(Pattern.matches("[amn]?",""));
-	System.out.println(Pattern.matches("[amn]+","a"));
+	System.out.println(Pattern.matches("[amn]+","a"));//true (a or m or n once or more times) 
 	System.out.println(Pattern.matches("[amn]+","aammn"));
 	System.out.println(Pattern.matches("[amn]+",""));
-	System.out.println(Pattern.matches("[amn]*","aa"));
+	System.out.println(Pattern.matches("[amn]*","aa"));//true (a or m or n may come zero or more times)
 	}
 
 }
