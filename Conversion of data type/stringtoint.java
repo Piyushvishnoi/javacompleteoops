81ec3174a574b1f4416b43df21ff746e3701cd9b
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-08 23:41:27 +0530

    String to integer using parse keyword

---
+++ Conversion of data type/stringtoint.java
@@ -0,0 +1,13 @@
+package javaconversion;
+
+//Java Program to demonstrate the conversion of String into int  
+//using Integer.parseInt() method  
+public class stringtoint{  
+public static void main(String args[]){  
+//Declaring String variable  
+String s="200";  
+//Converting String into int using Integer.parseInt()  
+int i=Integer.parseInt(s);  
+//Printing value of i  
+System.out.println(i);  
+}}  
