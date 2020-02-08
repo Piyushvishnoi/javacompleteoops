e2c65bfcafc1101f3d80566b1346b63c0185fda4
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-08 23:43:40 +0530

    number format exception

---
+++ Conversion of data type/numberformatexception.java
@@ -0,0 +1,9 @@
+package javaconversion;
+
+//Java Program to demonstrate the case of NumberFormatException  
+public class numberformatexception{  
+public static void main(String args[]){  
+String s="hello";  
+int i=Integer.parseInt(s);  
+System.out.println(i);  
+}}
