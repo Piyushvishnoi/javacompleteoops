bb3532298a105e64764f34fc249b05843ba5d1d5
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-09 23:40:03 +0530

    subarray concept in java

---
+++ hacker rank topics/subarrayinjava.java
@@ -0,0 +1,17 @@
+package hackerranktopics;
+
+import java.util.Arrays;
+public class subarrayinjava {
+   public static void main(String[] args) {
+      int[] array = new int[] {1, 2, 3, 4, 5};
+      int[] subArray = Arrays.copyOfRange(array, 0, 2);
+      System.out.println("Array: ");
+      for(int i = 0; i < array.length; i++) {
+         System.out.print(array[i] + " ");
+      }
+      System.out.println("\nSub array: ");
+      for(int i = 0; i < subArray.length; i++) {
+         System.out.print(subArray[i] + " ");
+      }
+   }
+}
