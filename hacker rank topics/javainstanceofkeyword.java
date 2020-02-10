7b7e767de90001eac9817561f8e44c79399e9213
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-10 23:41:09 +0530

    instance of keyword in java

---
+++ hacker rank topics/javainstanceofkeyword.java
@@ -0,0 +1,32 @@
+package hackerranktopics;
+
+//relationship 
+class Parent {  } 
+class Child extends Parent { } 
+
+class javainstanceofkeyword 
+{ 
+ public static void main(String[] args) 
+ { 
+     Child cobj = new Child(); 
+
+     // A simple case 
+     if (cobj instanceof Child) 
+        System.out.println("cobj is instance of Child"); 
+     else
+        System.out.println("cobj is NOT instance of Child"); 
+
+     // instanceof returns true for Parent class also  
+     if (cobj instanceof Parent) 
+        System.out.println("cobj is instance of Parent"); 
+     else
+        System.out.println("cobj is NOT instance of Parent"); 
+
+     // instanceof returns true for all ancestors (Note : Object 
+     // is ancestor of all classes in Java)  
+     if (cobj instanceof Object) 
+        System.out.println("cobj is instance of Object"); 
+     else
+        System.out.println("cobj is NOT instance of Object");            
+ } 
+}
