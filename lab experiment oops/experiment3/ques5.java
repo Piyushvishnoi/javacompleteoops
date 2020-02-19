04313309c08294a5821cd13aea6fc67160ce4808
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-20 00:21:35 +0530

    question 5
    employee, manager

---
+++ lab experiment oops/experiment3/ques5.java
@@ -0,0 +1,51 @@
+package javalabexp;
+import java.util.Scanner;
+class employee{
+	public String name;
+	public Integer empid;
+	public Integer salary;
+	employee()
+	{}
+	employee(String name,int empid,int salary)
+	{
+		this.name=name;
+		this.empid=empid;
+		this.salary=salary;
+	}
+	String toreturnname()
+	{
+		return name;
+	}
+	Integer toreturnsalary()
+	{
+		return salary;
+	}
+	void toincreasesalary()
+	{
+System.out.println("Enter the percentage by which salary must be increased: ");
+	Scanner s=new Scanner(System.in);
+	Integer perc=s.nextInt();
+	salary=salary+(salary*perc/100);
+	System.out.println("Salary after increament: "+salary);
+	}
+	
+}
+class manager extends employee
+{ public String department;
+	manager()
+	{
+		System.out.println("Enter department name: ");
+		Scanner s=new Scanner(System.in);
+		String dep=s.nextLine();
+	}
+}
+public class ques5 {
+
+	public static void main(String[] args) {
+	employee obj=new employee("Piyush",50067083,100000); 
+System.out.println("name of the employee is: "+obj.toreturnname());
+System.out.println("salary of the emplyee is: "+obj.toreturnsalary());
+employee obj1=new manager();
+	}
+
+}
