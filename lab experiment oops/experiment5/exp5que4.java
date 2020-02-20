fbcb5284394b5cdaf699eec2b2aa8b6d33bb3501
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-20 18:34:08 +0530

    question 4

---
+++ lab experiment oops/experiment5/exp5que4.java
@@ -0,0 +1,16 @@
+package javalabexp;
+import java.util.Scanner;
+interface demo
+{
+	void division();
+	void modules();
+}
+
+public class exp5que4 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
