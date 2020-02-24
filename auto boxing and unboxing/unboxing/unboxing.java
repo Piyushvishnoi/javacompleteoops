94aef48f09fb3a796eacd4bc0e4b190c7d0f9bae
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-24 23:37:33 +0530

    Update unboxing.java

--- auto boxing and unboxing/unboxing/unboxing.java
+++ auto boxing and unboxing/unboxing/unboxing.java
@@ -4,7 +4,7 @@ public class unboxing {
 
 	public static void main(String[] args) {
 		Integer i=12;
-		int j=i;  
+		int j=i;  //here unboxing takes place i.e conversion of wrapper class to primitive implicitly
 		System.out.println(j);
 		
 		System.out.println(i);
