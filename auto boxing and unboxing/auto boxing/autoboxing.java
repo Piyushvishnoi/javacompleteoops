a7abdce67c1ad278cdbbb849fde5724914953ab1
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-24 23:31:22 +0530

    Update autoboxing.java

--- auto boxing and unboxing/auto boxing/autoboxing.java
+++ auto boxing and unboxing/auto boxing/autoboxing.java
@@ -5,7 +5,7 @@ public class autoboxing {
 	public static void main(String[] args) {
 		Integer i=2;
 		Integer j=new Integer(2);
-		if(i==j)
+		if(i==j)    //here address refered are different in both type of declaration of i and j
 		{
 			System.out.println("Same");
 		}
@@ -16,7 +16,7 @@ public class autoboxing {
 		}
 		Integer k=3;
 		Integer l=3;
-		if(k==l)
+		if(k==l)   //here if value is between -128 to 127 then the address assigned to both will be same
 		{
 			System.out.println("Same");
 		}
@@ -27,7 +27,7 @@ public class autoboxing {
 		}
 		Integer m=300;
 		Integer n=300;
-		if(m==n)
+		if(m==n)    ///Since value is above 127 , hence value of x and y are not same
 		{
 			System.out.println("Same");
 		}
