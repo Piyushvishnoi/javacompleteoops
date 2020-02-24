057808ceee424adef3bd334952058300eec5467a
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-24 23:36:29 +0530

    unboxing in java

---
+++ auto boxing and unboxing/unboxing/unboxing.java
@@ -0,0 +1,13 @@
+package autoboxingunboxing;
+
+public class unboxing {
+
+	public static void main(String[] args) {
+		Integer i=12;
+		int j=i;  
+		System.out.println(j);
+		
+		System.out.println(i);
+	}
+
+}
