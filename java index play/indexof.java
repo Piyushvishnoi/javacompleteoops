2dac0b061266a208209e528f738bd5520d8429c3
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-26 14:03:49 +0530

    all formats of Indexof in java

---
+++ java index play/indexof.java
@@ -0,0 +1,13 @@
+package indexoftypes;
+
+public class indexof {
+
+	public static void main(String[] args) {
+		String s="It is very goodish types";
+		System.out.println(s.indexOf("i"));
+		System.out.println(s.indexOf("i",4));
+		System.out.println(s.indexOf("good"));
+		System.out.println(s.indexOf("type"));
+	}
+
+}
