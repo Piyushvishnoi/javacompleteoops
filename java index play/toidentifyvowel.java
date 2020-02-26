5beeb475276772569a78fa5b20894f12a5c990ef
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-26 14:06:57 +0530

    to find vowels or chars

---
+++ java index play/toidentifyvowel.java
@@ -0,0 +1,24 @@
+package indexoftypes;
+import java.util.Scanner;
+public class toidentifyvowel {
+
+	public static boolean vowel(char ch)
+	{
+		return "aeiouAEIOU".indexOf(ch)>=0;
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter Character: ");
+		char singlechar=s.next().charAt(0);
+		
+		boolean isvowel=vowel(singlechar);
+		if(isvowel)
+			System.out.println("vowel");
+		else
+		{
+			System.out.println("constant");
+		}
+
+	}
+
+}
