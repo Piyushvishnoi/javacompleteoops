6638f80d7e9d18d05f444e3fb2a7bbff34eeb324
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-26 14:51:32 +0530

    Update exp5que4.java

--- lab experiment oops/experiment5/exp5que4.java
+++ lab experiment oops/experiment5/exp5que4.java
@@ -2,28 +2,28 @@ package javalabexp;
 import java.util.Scanner;
 interface demooverriding
 {
-	void division();
-	default void modules()
+	void division();     //abstract methods
+	default void modules()   //default methods which can be overridden
 	{
 		System.out.println("in parent interface and in module method");
 	}
 }
-interface extendeddemo extends demooverriding
+interface extendeddemo extends demooverriding   //extending another interface
 {
-	default void modules()
+	default void modules()   //overriding default method of parent interface
 	{
 		System.out.println("in  subinterface and in module method");
 	}
 }
 
-public class exp5que4 implements extendeddemo {
+public class exp5que4 implements extendeddemo {    //implementing subinterface
 
 public void division()
 {
 	System.out.println("in division method");
 }
 	public static void main(String[] args) {
-		exp5que4 obj=new exp5que4();
+		exp5que4 obj=new exp5que4();      //making object because static to non-static can't be called
 		System.out.println("Calling division function");
 obj.division();
 System.out.println("Calling module function");
