c98c5055ea9eec23e901dec76df3d26df0717a14
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-26 14:05:36 +0530

    Update indexof.java

--- java index play/indexof.java
+++ java index play/indexof.java
@@ -4,9 +4,9 @@ public class indexof {
 
 	public static void main(String[] args) {
 		String s="It is very goodish types";
-		System.out.println(s.indexOf("i"));
-		System.out.println(s.indexOf("i",4));
-		System.out.println(s.indexOf("good"));
+		System.out.println(s.indexOf("i"));  //tell that value of i is present at which index
+		System.out.println(s.indexOf("i",4));  //start searching from the fourth position to search the i element
+		System.out.println(s.indexOf("good"));  //to find a substring in the string
 		System.out.println(s.indexOf("type"));
 	}
 
