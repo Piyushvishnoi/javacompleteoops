b44c732921bcbce2f60daf470a074126a5a3170f
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-26 15:16:49 +0530

    Update exp5que2.java

--- lab experiment oops/experiment5/exp5que2.java
+++ lab experiment oops/experiment5/exp5que2.java
@@ -1,23 +1,23 @@
 package javalabexp;
 import java.util.Scanner;
-interface A
+interface A      //defining interface
 {
-	void method1(); //these methods are abstract by default
+	void method1();    //by default, abstract function
 	void method2();
 }
-class MyClass implements A. //implementing interface A
+class MyClass implements A   //implementing interface
 {
-	public void method1()  //definition given to abstract methods
+	public void method1()    //defining abstract function
 	{
 	System.out.println("method 1 defines addition function");
-Scanner s=new Scanner(System.in);     //object of scanner class
+Scanner s=new Scanner(System.in);  //making object for scanner
 System.out.println("Enter two numbers a and b for addition:");
 Integer a=s.nextInt();
 Integer b=s.nextInt();
 Integer sum=a+b;
 System.out.println("Sum is:"+sum);
 	}
-	public void method2()
+	public void method2()   //defining abstract function
 	{
 		System.out.println("method 1 defines subtraction function");
 		Scanner s=new Scanner(System.in);
@@ -28,7 +28,7 @@ System.out.println("Sum is:"+sum);
 		System.out.println("Subtraction is:"+subtraction);	
 	}
 }
-public class exp5que2 {
+public class exp5que2 {  //exp5que2 has-A relationship with class MyClass
 
 	public static void main(String[] args) {
 		MyClass obj=new MyClass();
