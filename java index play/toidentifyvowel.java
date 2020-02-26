12e79135d5d8ecab31272ae825e207cdb128520c
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-26 14:08:30 +0530

    Update toidentifyvowel.java

--- java index play/toidentifyvowel.java
+++ java index play/toidentifyvowel.java
@@ -4,7 +4,7 @@ public class toidentifyvowel {
 
 	public static boolean vowel(char ch)
 	{
-		return "aeiouAEIOU".indexOf(ch)>=0;
+		return "aeiouAEIOU".indexOf(ch)>=0;   //return something if present in aeiou
 	}
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
