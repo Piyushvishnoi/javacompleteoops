dceb03bfb84ccbaf5f97bce029ea890af4cd03fe
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-27 22:19:39 +0530

    package to be imported

---
+++ lab experiment oops/experiment 6/ques2/exp6que2.java
@@ -0,0 +1,11 @@
+package javalabexp;
+import balance.Account;
+public class exp6que2 {
+
+	public static void main(String[] args) {
+		Account obj=new Account();
+		obj.display_balance();
+
+	}
+
+}
