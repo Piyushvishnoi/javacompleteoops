3f075779a4dab605ac5b70f2e3075f2faf17d91c
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-28 12:34:31 +0530

    exp 7 demonstration of execution handling

---
+++ lab experiment oops/experiment 7/exp7que1.java
@@ -0,0 +1,28 @@
+package javalabexp;
+import java.lang.ArrayIndexOutOfBoundsException;
+import java.util.Scanner;
+class exp7
+{
+	String[] name=new String[5];
+	Integer[] rollno=new Integer[5];
+}
+
+public class exp7que1  {
+
+	public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
+		exp7 obj=new exp7();
+		for(int i=0;i<obj.name.length;i++)
+		{
+			try {
+				Scanner s=new Scanner(System.in);
+				obj.name[i]=s.nextLine();
+			}
+		
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("index is exceeding");
+		}
+		}
+	}
+
+}
