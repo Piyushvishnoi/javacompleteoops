8068898aab73e14dc45f49de4c3951c5e86b36c2
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-25 23:45:29 +0530

    Update vectordemo.java

--- vectors in java/vectordemo.java
+++ vectors in java/vectordemo.java
@@ -20,5 +20,25 @@ System.out.println("capacity is: " + v.capacity()); //default capacity of vector
 String s=v.elementAt(2);
 System.out.println(s);
 	}
+	// boolean function
+	import java.util.*; 
+class Vector_demo { 
+    { 
+        ArrayList arr = new ArrayList(); 
+        arr.add(3); 
+        arr.add("geeks"); 
+        arr.add("forgeeks"); 
+        arr.add(4); 
+  
+        // createn default vector 
+        Vector v = new Vector(); 
+  
+        // copying all element of array list int0 vector 
+        v.addAll(arr); 
+  
+        // checking vector v 
+        System.out.println("vector v:" + v); 
+    } 
+}
 
 }
