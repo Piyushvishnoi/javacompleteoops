e28d094d67718af387f31df490f5d80cb8039c5b
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-31 21:52:25 +0530

    Update collectiondemo.java

--- collections in java/collectiondemo.java
+++ collections in java/collectiondemo.java
@@ -1,6 +1,7 @@
 //we cannot use index no in collection and arraylist
 // Before Collection Framework (or before JDK 1.2) was introduced, the standard methods for grouping Java objects (or collections) were Arrays or Vectors or Hashtables. 
 //All of these collections had no common interface.
+//Collection is by default object type 
 package collectionandgenerics;
 import java.util.ArrayList;  //package for ArrayList
 import java.util.Collection; //package for collection
