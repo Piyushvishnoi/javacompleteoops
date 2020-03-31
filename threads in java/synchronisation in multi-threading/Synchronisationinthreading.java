eecbf8d257df4eee30e7a680761d9829a802953b
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-31 20:34:52 +0530

    Update Synchronisationinthreading.java

--- threads in java/synchronisation in multi-threading/Synchronisationinthreading.java
+++ threads in java/synchronisation in multi-threading/Synchronisationinthreading.java
@@ -11,9 +11,9 @@ public class Synchronisationinthreading {
 	public static void main(String[] args) throws Exception {
 		counter c=new counter();
 		//Runnable obj1=new Runnable();
-		Thread t1=new Thread(new Runnable()
+		Thread t1=new Thread(new Runnable()      //implementing runnable interface
 				{
-				public void run()
+				public void run()      //defining run function
 				{
 			for(Integer i=0;i<1000;i++)
 			{
@@ -21,8 +21,8 @@ public class Synchronisationinthreading {
 			}
 				}
 				});
-		t1.start();
-		t1.join();
+		t1.start();           //starting the thread
+		t1.join();            //joining the thread into main thread
 		System.out.println(c.count);
 	}
 
