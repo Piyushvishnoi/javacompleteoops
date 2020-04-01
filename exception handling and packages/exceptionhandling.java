b9af47338a032e81975f17de522bdeea962dabb7
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-01 23:44:35 +0530

    Update exceptionhandling.java

--- exception handling and packages/exceptionhandling.java
+++ exception handling and packages/exceptionhandling.java
@@ -3,13 +3,13 @@ package p1;
 public class exceptionhandling {
 
 	public static void main(String[] args) {
-try
+try  //exception part will be in this try block
 {
 	int i=2;
 	int j=0;
 	int k=i/j;
 }
-catch(Exception e)
+catch(Exception e)   //catch is used to handle that exception which is found in try block
 {
 	System.out.println("error");
 }
