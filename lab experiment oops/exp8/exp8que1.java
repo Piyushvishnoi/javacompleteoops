5a63cfa6c3a100f8f6ba86a0568c7dbe47de3091
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-03 00:59:01 +0530

    Update exp8que1.java

--- lab experiment oops/exp8/exp8que1.java
+++ lab experiment oops/exp8/exp8que1.java
@@ -5,6 +5,9 @@ public class exp8que1 {
 	public static void main(String[] args) {
 		String s="I am Piyush Vishnoi. I am currently persuing my Btech degree from Upes,dehradun.";
 		//for a single character
+		//indexof gives first occurenece 
+		//lastIndexof gives lastindex occurence
+		//indexOf with an index no will give the occurence after certain index no
 		System.out.println("First index at which m occurs:"+s.indexOf("m"));
 		System.out.println("last index at which m occurs:"+s.lastIndexOf("m"));
 		System.out.println("First index at which m occurs after some index:"+s.indexOf("m",5));
