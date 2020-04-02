43caf3abf9c72a018ed90c6eaa695d4abc6eaa54
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-03 00:57:27 +0530

    exp8ques1

---
+++ lab experiment oops/exp8/exp8que1.java
@@ -0,0 +1,18 @@
+package javalabexp;
+
+public class exp8que1 {
+
+	public static void main(String[] args) {
+		String s="I am Piyush Vishnoi. I am currently persuing my Btech degree from Upes,dehradun.";
+		//for a single character
+		System.out.println("First index at which m occurs:"+s.indexOf("m"));
+		System.out.println("last index at which m occurs:"+s.lastIndexOf("m"));
+		System.out.println("First index at which m occurs after some index:"+s.indexOf("m",5));
+//for a substring
+		String sub="am";
+		System.out.println("First index at which " + sub + " occurs:"+s.indexOf(sub));
+		System.out.println("last index at which "+sub+ " occurs:"+s.lastIndexOf(sub));
+		System.out.println("First index at which " +sub+ " occurs after some index:"+s.indexOf(sub,5));	
+	}
+
+}
