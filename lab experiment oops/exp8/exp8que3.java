862e804fd656ae34dacb955a3f1b1c2c08a520da
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-03 09:47:35 +0530

    lower to upper case without using inbuiltmehods

---
+++ lab experiment oops/exp8/exp8que3.java
@@ -0,0 +1,32 @@
+package javalabexp;
+
+import java.util.Scanner;
+
+public class exp8que3 {
+    static String LowerToUpper(String s) {
+        String result = ("");
+        char ch = ' ';
+        for (int i = 0; i < s.length(); i++) {
+            
+            //check valid alphabet and it is in lowercase
+            if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                ch = (char)(s.charAt(i) - 32);
+            }
+            //else keep the same alphabet or any character
+            else {
+                ch = (char)(s.charAt(i));
+            }
+            
+            result =result + ch; // concatenation, append c to result
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+    	Scanner input=new Scanner(System.in);
+    	System.out.println("Enter lower case string: ");
+    	String s=input.nextLine();
+    	System.out.println("upper case case string: "+LowerToUpper((s)));
+       
+    }
+}
