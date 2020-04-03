012bbd08bb1c8b0e4156192f904571a05d87b9f5
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-03 10:41:47 +0530

    Update exp9que6.java

--- lab experiment oops/experiment 9/collection/exp9que6.java
+++ lab experiment oops/experiment 9/collection/exp9que6.java
@@ -5,13 +5,13 @@ import java.util.HashMap;
 public class exp9que6 {
 
 	public static void main(String[] args) {
-		HashMap <Integer,String> obj=new HashMap<>();
-obj.put(1, "Piyush");
+		HashMap <Integer,String> obj=new HashMap<>();  //making object of HashMap
+obj.put(1, "Piyush");    //Adding values in HashMap
 obj.put(2, "Vishnoi");
 obj.put(3, "Khushi");
-System.out.println(obj.containsKey(3));
-System.out.println(obj.containsValue("Vishnoi"));
-System.out.println(obj.keySet());
+System.out.println(obj.containsKey(3));    //checking whether HashMap contains a particular key or not
+System.out.println(obj.containsValue("Vishnoi"));  //checking whether HashMap contains a particular value or not
+System.out.println(obj.keySet());  //to get all the keys from the HashMap
 System.out.println(obj);
 	}
 
