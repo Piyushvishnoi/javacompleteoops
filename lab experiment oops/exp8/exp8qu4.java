df55c18856f2780f1beed0a8f4bfd9ec1767dbfb
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-03 09:49:26 +0530

    various string operations

---
+++ lab experiment oops/exp8/exp8qu4.java
@@ -0,0 +1,20 @@
+package javalabexp;
+
+public class exp8qu4 {
+
+	public static void main(String[] args) {
+		Integer count=0;
+		String s=new String("Ameesha Agrawal");
+		System.out.println("length of name is: "+s.length());
+		for(int i=0;i<s.length();i++)
+		{
+		if(s.charAt(i)=='a' && s.charAt(i)=='a')
+		{
+			count++;
+			System.out.println("element a is found at: "+i+ " position");	
+		}
+		}
+		System.out.println("element a is found "+count+ " times");
+	}
+
+}
