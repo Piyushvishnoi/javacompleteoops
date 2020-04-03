6ab91fcabcdd691186f0009aa96ff02d788421c2
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-03 09:54:07 +0530

    Update exp8qu4.java

--- lab experiment oops/exp8/exp8qu4.java
+++ lab experiment oops/exp8/exp8qu4.java
@@ -4,11 +4,11 @@ public class exp8qu4 {
 
 	public static void main(String[] args) {
 		Integer count=0;
-		String s=new String("Ameesha Agrawal");
-		System.out.println("length of name is: "+s.length());
+		String s=new String("Appu Armania");
+		System.out.println("length of name is: "+s.length());   //using length pre-defined method
 		for(int i=0;i<s.length();i++)
 		{
-		if(s.charAt(i)=='a' && s.charAt(i)=='a')
+		if(s.charAt(i)=='a' && s.charAt(i)=='a')     //checking condition
 		{
 			count++;
 			System.out.println("element a is found at: "+i+ " position");	
