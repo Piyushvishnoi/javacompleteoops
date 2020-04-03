e09e28817f055d3080305871af1eb14d0c49207e
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-03 10:27:32 +0530

    10 threads synchronised

---
+++ lab experiment oops/experiment 9/exp9que3.java
@@ -0,0 +1,49 @@
+package javalabexp;
+class Counter1 
+{ Integer count=0;
+	public  synchronized void increament()
+	{
+			count++;
+	}
+}
+public class exp9que3 {
+
+	public static void main(String[] args) throws Exception {
+		Counter1 c=new Counter1();
+		//Runnable obj1=new Runnable();
+		Thread t1=new Thread(new Runnable()
+				{
+		
+				public void run()
+				{
+					
+			for(Integer i=0;i<1000;i++)
+			{
+				c.increament();
+			}
+			
+				}
+				});
+		Thread t2=new Thread(new Runnable()
+		{
+		public void run()
+		{
+	for(Integer i=0;i<1000;i++)
+	{
+		c.increament();
+	}
+		}
+		});
+		t1.start();
+		t1.setName("Thread1");
+		System.out.println(t1.getName());
+		t1.join();
+		t2.start();
+		t1.setName("Thread2");
+		System.out.println(t1.getName());
+		t2.join();
+		System.out.println(c.count);
+	}
+
+}
+
