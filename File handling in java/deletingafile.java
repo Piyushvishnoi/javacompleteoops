7c6e57a86f1df37bf76060902c2d58de45d5ab79
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-09 00:01:07 +0530

    Update deletingafile.java

--- File handling in java/deletingafile.java
+++ File handling in java/deletingafile.java
@@ -1,11 +1,12 @@
 package filehandling;
-
+ 
 import java.io.File;
 
 public class deletingafile {
 
 	public static void main(String[] args) {
 		File obj=new File("File1.txt");
+		//deleting whole file 
 		if(obj.delete())
 		{
 			System.out.println("File got deleted with name: " + obj.getName());
