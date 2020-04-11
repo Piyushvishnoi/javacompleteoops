3dc990c943dc82e4a12693d4efd7ed180b451aed
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-11 12:42:27 +0530

    adding servlet

---
+++ Advanced java/Servlet/addservelet.java
@@ -0,0 +1,20 @@
+package newpackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class addservelet extends HttpServlet
+{
+public void service(HttpServletRequest req,HttpServletResponse res) throws IOException
+{
+	int i=Integer.parseInt(req.getParameter("num1"));
+	int j=Integer.parseInt(req.getParameter("num2"));
+	int k= i+j;
+	PrintWriter obj=res.getWriter();
+	obj.println("result is: "+k);
+	}
+}
