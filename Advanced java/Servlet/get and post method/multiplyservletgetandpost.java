09c119197851689b30bfe18411528e50e631143e
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-14 20:33:08 +0530

    Update multiplyservletgetandpost.java

--- Advanced java/Servlet/get and post method/multiplyservletgetandpost.java
+++ Advanced java/Servlet/get and post method/multiplyservletgetandpost.java
@@ -8,15 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class multiplyservletgetandpost extends HttpServlet {
-public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException
+public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException //throwing Input Output exception
 {
-int i=Integer.parseInt(req.getParameter("num1"));
+int i=Integer.parseInt(req.getParameter("num1"));    //converting string to Int
 int j=Integer.parseInt(req.getParameter("num2"));
 int k=i*j;
 PrintWriter out=res.getWriter();
 out.println("Result is: "+k);
 }
-public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException  //doGet method
 {
 int i=Integer.parseInt(req.getParameter("num1"));
 int j=Integer.parseInt(req.getParameter("num2"));
