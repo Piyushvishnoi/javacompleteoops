4f5202fe47e989f45f13421632f0f6138fd5a370
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-14 23:08:35 +0530

    Update stringtoint.java

--- Conversion of data type/stringtoint.java
+++ Conversion of data type/stringtoint.java
@@ -5,7 +5,7 @@ package javaconversion;
 public class stringtoint{  
 public static void main(String args[]){  
 //Declaring String variable  
-String s="200";   
+String s="200";    
 //Converting String into int using Integer.parseInt()  
 int i=Integer.parseInt(s);  
 //Printing value of  i  
