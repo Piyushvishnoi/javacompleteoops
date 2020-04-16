1e7f07f28a464867d06bec0e23baa01d08549a37
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-17 05:21:35 +0530

    Cookie is used to pass value

---
+++ Advanced java/Servlet/session and cookie/Cookie method/sendRedirectmethod.java
@@ -0,0 +1,22 @@
+package newpackage;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class sendRedirectmethod extends HttpServlet {
+public  void service(HttpServletRequest req,HttpServletResponse res) throws IOException
+{
+	int i=Integer.parseInt(req.getParameter("num1"));
+	int j=Integer.parseInt(req.getParameter("num2"));
+	int k=i+j;
+	PrintWriter obj=res.getWriter();
+	obj.println("Sum is: "+ k);
+	Cookie cookie=new Cookie("k",k+"");
+	res.addCookie(cookie);
+	res.sendRedirect("sq");
+}
+}
