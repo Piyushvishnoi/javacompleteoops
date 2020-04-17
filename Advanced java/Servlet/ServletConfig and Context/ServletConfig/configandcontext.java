2105116e3d4c563736ed07ac5c9f38e9b2b30ad4
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-17 10:44:35 +0530

    ServletContext

---
+++ Advanced java/Servlet/ServletConfig and Context/ServletConfig/configandcontext.java
@@ -0,0 +1,19 @@
+package newpackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class configandcontext extends HttpServlet {
+protected void service(HttpServletRequest req,HttpServletResponse res) throws IOException {
+	PrintWriter obj=res.getWriter();
+	obj.print("hi");
+	ServletConfig ctx=getServletConfig();
+	String str=ctx.getInitParameter("name");
+	obj.println(str);
+	}
+}
