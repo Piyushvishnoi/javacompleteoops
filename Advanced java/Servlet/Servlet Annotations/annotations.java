9147b1370d90060bbb32666e81ec30042a35bb17
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-17 23:46:48 +0530

    annotation servlets

---
+++ Advanced java/Servlet/Servlet Annotations/annotations.java
@@ -0,0 +1,20 @@
+package newpackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/add")
+public class annotations extends HttpServlet {
+	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+	int i=Integer.parseInt(req.getParameter("num1"));
+	int j=Integer.parseInt(req.getParameter("num2"));
+	int k=i*j;
+	PrintWriter out=res.getWriter();
+	out.println("Result is: "+k);
+	}
+}
