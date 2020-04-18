6858ffa3e2579503d2e94a91c3259b079ca7f2fa
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-18 23:48:37 +0530

    to take screen shot through java program

---
+++ fun patterns in java/taking screenshot and recording audio/totakescreenshots.java
@@ -0,0 +1,32 @@
+package newpackage;
+
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class totakescreenshots {
+public static final long serialversionuid=1L;
+	public static void main(String[] args) {
+		try {
+			Thread.sleep(120);
+			Robot r=new Robot();
+			String path="/Users/piyushvishnoi/Desktop/screenshot/";
+			Rectangle capture=new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+			BufferedImage Image=r.createScreenCapture(capture);
+			ImageIO.write(Image,"jpg",new File(path));
+			System.out.println("Screenshot saved");
+		}
+		catch(AWTException | IOException | InterruptedException ex)
+		{
+			System.out.println(ex);
+		}
+
+	}
+
+}
