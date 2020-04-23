2ff8e51d9aafdfd9ab3ce6f09919060c06f9dbcf
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-23 17:24:15 +0530

    calculator used

---
+++ lab experiment oops/exp 11/question 4/calculator.java
@@ -0,0 +1,36 @@
+package labexperiment;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class calculator extends HttpServlet{
+public  void doget(HttpServletRequest req,HttpServletResponse res) throws IOException
+{
+	PrintWriter obj=res.getWriter();
+	int i=Integer.parseInt(req.getParameter("num1"));
+	int j=Integer.parseInt(req.getParameter("num2"));
+	String operation=req.getParameter("op");
+	if(operation.equals("Addition"))
+	{
+		obj.println(i+j);
+	}
+	else if(operation.equals("Subtraction"))
+	{
+		obj.println(i-j);
+	}
+	else if(operation.equals("Multiplication"))
+	{
+		obj.println(i*j);
+	}
+	else if(operation.equals("Division"))
+	{
+		obj.println(i/j);
+	}
+	else {
+		obj.println("not matching any operation");}
+}
+}
