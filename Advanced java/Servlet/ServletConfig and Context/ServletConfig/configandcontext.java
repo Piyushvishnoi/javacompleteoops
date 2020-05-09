68d564cb140b6b5a413339178ed519d0546ed162
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-05-09 08:48:56 +0530

    Update configandcontext.java

--- Advanced java/Servlet/ServletConfig and Context/ServletConfig/configandcontext.java
+++ Advanced java/Servlet/ServletConfig and Context/ServletConfig/configandcontext.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter; 
 
 import javax.servlet.ServletConfig;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServlet; 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
