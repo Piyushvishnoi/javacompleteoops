9100701e2b54eb33442e69548dc2641bda7e874b
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-05-09 08:49:07 +0530

    Update configandcontext.java

--- Advanced java/Servlet/ServletConfig and Context/ServletConfig/configandcontext.java
+++ Advanced java/Servlet/ServletConfig and Context/ServletConfig/configandcontext.java
@@ -6,7 +6,7 @@ import java.io.PrintWriter;
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet; 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse; 
 
 public class configandcontext extends HttpServlet {
 protected void service(HttpServletRequest req,HttpServletResponse res) throws IOException {
