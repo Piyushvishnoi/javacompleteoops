ab106079a57be127220f2302e58953c5d3614879
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-06-26 00:11:14 +0530

    Update numberformatexception.java

--- Conversion of data type/numberformatexception.java
+++ Conversion of data type/numberformatexception.java
@@ -2,7 +2,7 @@ package javaconversion;
 //If you don't have numbers in string literal, calling Integer.parseInt() or Integer.valueOf() methods throw NumberFormatException
 //Java Program to demonstrate the case of NumberFormatException  
 public class numberformatexception{   
-public static void main(String args[]){  
+public static void main(String args[]){   
 String s="hello";   
 int i=Integer.parseInt(s);     
 System.out.println(i);   
