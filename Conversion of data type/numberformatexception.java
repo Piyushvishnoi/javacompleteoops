1c4993e8c2cbe158eb1c77aa9e05b4b6f7111013
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-06-26 00:05:01 +0530

    Update numberformatexception.java

--- Conversion of data type/numberformatexception.java
+++ Conversion of data type/numberformatexception.java
@@ -3,7 +3,7 @@ package javaconversion;
 //Java Program to demonstrate the case of NumberFormatException  
 public class numberformatexception{  
 public static void main(String args[]){  
-String s="hello";  
+String s="hello";   
 int i=Integer.parseInt(s);   
 System.out.println(i);  
 }}
