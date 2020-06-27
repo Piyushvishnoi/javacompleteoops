922f4f9f6118d272a30cefad11f37f4791320d1b
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-06-28 01:52:41 +0530

    Update Writingtoafile.java

--- File handling in java/Writingtoafile.java
+++ File handling in java/Writingtoafile.java
@@ -5,7 +5,7 @@ import java.io.IOException;
   
 public class Writingtoafile {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 try
 {
 	FileWriter obj=new FileWriter("File1.txt");
