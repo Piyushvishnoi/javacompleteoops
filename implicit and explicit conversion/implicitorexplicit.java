15239a65371bc341e2eb6625f9e0d007cb1f9bf3
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-07-01 20:46:13 +0530

    implicit and explicit conversion

---
+++ implicit and explicit conversion/implicitorexplicit.java
@@ -0,0 +1,12 @@
+
+public class implicitorexplicit {
+
+	public static void main(String[] args) {
+		 int a=(int)5.5; // explicit conversion
+		 double b=5;//implicit conversion
+		 System.out.println(a);
+		 System.out.println(b);
+
+	}
+
+}
