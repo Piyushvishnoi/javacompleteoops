ac4f82e3c379f13b22ba1113855458f22a2740ad
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-07-04 02:02:34 +0530

    multiple catch blocks

---
+++ exception handling and packages/multiple catch blocks/multiplecatchblocks.java
@@ -0,0 +1,23 @@
+package exception;
+
+public class multiplecatchblocks {
+
+	public static void main(String arg[])
+	{
+		try {
+			int b[]=null;
+			
+			Integer i=0;
+			Integer j=1;
+			Integer k=j/i;
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("Index out of bound");
+		}
+		catch(Exception e)
+		{
+			System.err.println("Something is wrong");
+		}
+	}
+}
