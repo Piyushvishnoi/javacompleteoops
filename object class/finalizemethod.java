03f4065501fd7c4aefd5d0f3ad8675a48dd1ea6f
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-07-04 21:40:03 +0530

    Update finalizemethod.java

--- object class/finalizemethod.java
+++ object class/finalizemethod.java
@@ -6,7 +6,7 @@ public class finalizemethod {
 
 	public static void main(String[] args) {  
 	finalizemethod obj1=new finalizemethod();
-	System.out.println(obj1.hashCode());
+	System.out.println(obj1.hashCode()); 
 	obj1=null;
 	//calling garbage collector
 	System.gc();
