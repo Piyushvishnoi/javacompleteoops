64f341467b050ae7b71cccabfac28ecc6b4dcfdb
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-10-04 21:48:25 +0530

    Update annotations.java

--- Advanced java/Servlet/Servlet Annotations/annotations.java
+++ Advanced java/Servlet/Servlet Annotations/annotations.java
@@ -1,7 +1,6 @@
 package newpackage;
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
