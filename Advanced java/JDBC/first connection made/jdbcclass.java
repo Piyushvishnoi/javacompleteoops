9502fbb4f1ca0973c613436456b53ebe4c85e56a
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-12-14 23:57:05 +0530

    Update jdbcclass.java

--- Advanced java/JDBC/first connection made/jdbcclass.java
+++ Advanced java/JDBC/first connection made/jdbcclass.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;   
 public class jdbcclass {
 	public static void main(String[] args) throws Exception  
-	{
+	{ 
 		//Step 2: Registering JDBC drivers
 		Class.forName("com.mysql.jdbc.Driver");
 		//Step 3: Database url formation
