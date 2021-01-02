64aa35cff52ebb1d6192e6c673af72319870209c
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2021-01-02 23:00:19 +0530

    Update jdbcclass.java

--- Advanced java/JDBC/first connection made/jdbcclass.java
+++ Advanced java/JDBC/first connection made/jdbcclass.java
@@ -9,7 +9,7 @@ public class jdbcclass {
 	{  
 		//Step 2: Registering JDBC drivers
 		Class.forName("com.mysql.jdbc.Driver");
-		//Step 3: Database url formation
+		//Step 3: Database url formation 
 		Connection conn=DriverManager.getConnection("jdbc:mysql:///Db1","root","2338");
 		//step 4:Statement preperation 
 		Statement st=conn.createStatement();
