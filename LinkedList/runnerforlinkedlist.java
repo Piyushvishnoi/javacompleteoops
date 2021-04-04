a1007fd1e911bfe6a2d40250fddc43219bcb7b25
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2021-04-05 02:55:41 +0530

    not using default LinkedList fuction of java

---
+++ LinkedList/runnerforlinkedlist.java
@@ -0,0 +1,21 @@
+package new_java_programs;
+
+public class runnerforlinkedlist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+// Internal implementation of linked list
+ LinkedList list = new LinkedList();
+list.add(3);
+list.add(4,12);
+*/
+		linkedlist list=new linkedlist();
+		list.insert(5);
+		list.insert(12);
+		list.insert(22);
+		list.insert(98);
+list.show();
+	}
+
+}
