cf69cdb4e1db90e9d6ff0ed96e842407c8187cc9
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2021-04-29 00:03:59 +0530

    Update functionalinterface.java

--- interface in java/functionalinterface.java
+++ interface in java/functionalinterface.java
@@ -8,6 +8,6 @@ public class functionalinterface {
 	public static void main(String[] args) {
 fi d=() -> System.out.println("lambda expression"); // -> lambda expression can be only used with functional interface
 d.show(); 
-	}
+	} 
 
 }
